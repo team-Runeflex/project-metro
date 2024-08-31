5d4f4390ee84c9e97f31631dc36f344b2eff767b
sgoo <ppiredragonz@naver.com>
2024-08-31 20:24:33 +0900

    PlayerCharacterData.cs Update and EnemyData Update etc..

--- Assets/Script/Player/PlayerState.cs
+++ Assets/Script/Player/PlayerState.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public PlayerCharacterData PCD;
     public float health;
     public float might = 1;
     
 
     private void Start()
     {
-        
+        health = PCD.Health;
     }
 
     private void Update()
