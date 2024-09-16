4c88ac474e01d7f3c544bf1fd75a032cd4c2cb76
sgoo <ppiredragonz@naver.com>
2024-09-17 00:19:58 +0900

    원거리 공격 발사, 데미지, 시간 지난 후 삭제 기능

--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -9,9 +9,14 @@ public class PlayerAttack : MonoBehaviour
     public Transform pos;
     public Vector2 boxSize;
     public float defaultDamage;
+    public PlayerCharacterData playerData;
+    public Transform PrefabsParent;
+    
     
     private void Update()
     {
+
+        
         if (curTime <= 0)
         {
             if (Input.GetKeyDown(KeyCode.X))
@@ -19,24 +24,33 @@ public class PlayerAttack : MonoBehaviour
                 Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0); //히트박스 생성
                 foreach (Collider2D collider in collider2Ds)
                 {
+                    
                     if (collider.CompareTag("Enemy"))
                     {
                         collider.GetComponent<EnemyState>().TakeDamage(defaultDamage);
                     }
+                    curTime = coolTime;
                 }
-                curTime = coolTime;
             }
 
             if (Input.GetKeyDown(KeyCode.V))
             {
-                
+                AttackRanged();
             }
         }
+        
         else
         {
             curTime -= Time.deltaTime;
         }
     }
+    private void AttackRanged()
+    {
+        // 프리팹을 pos.position 위치에 생성 (회전은 Quaternion.identity)
+        GameObject rangedInstance = Instantiate(playerData.DefaultRangedData.RangedPrefab, gameObject.transform.position, Quaternion.identity);
+    }
+
+
 
     private void OnDrawGizmos()
     {
